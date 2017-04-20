de31f73d5c83129197c8a0e40d6ddc1adacf9204
QOO <p61402@gmail.com>
2017-04-20 08:56:42 +0800

    Add files via upload

--- Problem04.cpp
+++ Problem04.cpp
@@ -1,35 +1,35 @@
 #include <iostream> 
-using namespace std; 
- 
-int main() 
-{ 
-   int num_of_case; 
-   cin >> num_of_case; 
- 
- int num_of_store; 
-  while (num_of_case--) 
-  { 
-      cin >> num_of_store; 
- 
-        int *a = new int[num_of_store]; 
-        int width = 0; 
-     int total_length = 0; 
-      for (int i = 0; i < num_of_store; i++) 
-      { 
-          cin >> a[i]; 
- 
-            if (i >= 1) 
-         { 
-              width += a[i - 1]; 
-         } 
- 
-         total_length += (width + a[i] / 2); 
-        } 
- 
-     cout << total_length << endl; 
- 
-     delete[]a; 
- } 
- 
- return 0; 
+using namespace std;
+
+int main()
+{
+	int num_of_case;
+	cin >> num_of_case;
+
+	int num_of_store;
+	while (num_of_case--)
+	{
+		cin >> num_of_store;
+
+		int *a = new int[num_of_store];
+		int width = 0;
+		int total_length = 0;
+		for (int i = 0; i < num_of_store; i++)
+		{
+			cin >> a[i];
+
+			if (i >= 1)
+			{
+				width += a[i - 1];
+			}
+
+			total_length += (width + a[i] / 2);
+		}
+
+		cout << total_length << endl;
+
+		delete[]a;
+	}
+
+	return 0;
 }
